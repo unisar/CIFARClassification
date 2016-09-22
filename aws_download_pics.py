83d8fd096025b7a32751782a17d8e0588dfab202
iamshang1 <emailshang@gmail.com>
2016-09-22 14:25:23 -0400

    adding small image dataset

--- aws_download_pics.py
+++ aws_download_pics.py
@@ -1,36 +1,21 @@
 import subprocess
+import urllib
 
 '''
-NOTE:
-Before running set AWS_ACCESS_ID and AWS_SECRET_KEY environment variables
+download train and test images from S3 to local drive
 '''
 
 #settings:
-X_train = "./data/X_train_small.txt"
-X_test = "./data/X_test_small.txt"
-
+files = ["./data/X_small_test.txt","./data/X_small_train.txt"]
 
 #download files
-with open(X_train,'r') as f:
-    filenames = f.readlines()
+filenames = []
+for file in files:
+    with open(file,'r') as f:
+        filenames += f.readlines()
 
 for i in xrange(len(filenames)):
     file = filenames[i].replace('\n','').strip()
-    cmd = 'wget --directory-prefix=binaries/train https://s3.amazonaws.com/eds-uga-csci8360/data/project2/binaries/'+file+'.bytes'
-    #cmd = 'aws s3 cp s3://eds-uga-csci8360/data/project2/binaries/%s.bytes data/binaries/%s.bytes' % (file,file)
-    print 'Getting file %i of %i: %s' % (i+1,len(filenames),file)
-    push=subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
-    print push.returncode
-
-
-with open(X_test,'r') as f:
-    filenames = f.readlines()
-
-for i in xrange(len(filenames)):
-    file = filenames[i].replace('\n','').strip()
-    #cmd = 'aws s3 cp s3://eds-uga-csci8360/data/project2/binaries/%s.bytes data/binaries/%s.bytes' % (file,file)
-    cmd = 'wget --directory-prefix=binaries/test https://s3.amazonaws.com/eds-uga-csci8360/data/project2/binaries/'+file+'.bytes'
-    print 'Getting file %i of %i: %s' % (i+1,len(filenames),file)
-    push=subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
-    print push.returncode
-    
+    print 'Getting file %i of %i: %s' % (i+1,len(filenames),file)    
+    imgopen = urllib.URLopener()
+    imgopen.retrieve('https://s3.amazonaws.com/eds-uga-csci8360/data/project3/images/%s.png' % file, "./data/images/%s.png" % file)
