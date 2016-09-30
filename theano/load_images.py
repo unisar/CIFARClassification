564f842ba75075e90206c3d92d7aa054130ea672
iamshang1 <emailshang@gmail.com>
2016-09-30 16:50:35 -0400

    simplified code for horizontal flip of images

--- theano/load_images.py
+++ theano/load_images.py
@@ -27,26 +27,3 @@ for i in xrange(len(inputs)):
     final = np.concatenate((final,inputs[i]),axis=0)
     
 np.save('X_train', final)
-
-inputs = []
-
-input = np.empty((0,3,32,32))
-for i in xrange(len(filenames)):
-    if i % 1000 == 0:
-        inputs.append(input)
-        input = np.empty((0,3,32,32))
-    print 'Loading file %i of %i:' % (i+1,len(filenames))
-    file = filenames[i].replace('\n','').strip()
-    image = Image.open("../data/images/%s.png" % file).transpose(Image.FLIP_LEFT_RIGHT)
-    img = np.array(image,dtype='float64')/256
-    img = img.transpose(2, 0, 1).reshape(1,3,32,32)
-    input = np.concatenate((input,img),axis=0)
-    image.close()
-inputs.append(input)
-
-final = np.empty((0,3,32,32))
-for i in xrange(len(inputs)):
-    print 'Combining chunks %i of %i:' % (i+1,len(inputs))
-    final = np.concatenate((final,inputs[i]),axis=0)
-    
-np.save('X_train_flipped', final)
