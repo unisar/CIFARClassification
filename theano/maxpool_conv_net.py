eb51a21393829abaea583b45367d74bebe946c1a
iamshang1 <emailshang@gmail.com>
2016-10-06 11:34:39 -0400

    added ELU units to theano and tf models

--- theano/maxpool_conv_net.py
+++ theano/maxpool_conv_net.py
@@ -3,12 +3,14 @@ import theano
 import theano.tensor as T
 from theano.tensor.nnet import conv2d
 from theano.tensor.signal import downsample
-from theano.tensor.shared_randomstreams import RandomStreams
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 import sys
 import glob
 import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
 import random
+
+srng = RandomStreams()
 
 input = np.load('X_train_zca.npy')   
 labels = np.genfromtxt('../data/y_train.txt')
@@ -18,20 +20,29 @@ X_train, X_test, y_train, y_test = train_test_split(input, labels, test_size=0.1
 convolutional_layers = 6
 feature_maps = [3,80,80,160,160,320,320]
 filter_shapes = [(3,3),(3,3),(3,3),(3,3),(3,3),(3,3)]
+image_shapes = [(24,24),(24,24),(12,12),(12,12),(6,6),(6,6)]
 feedforward_layers = 1
 feedforward_nodes = [2000]
 classes = 10
 
 class convolutional_layer(object):
-    def __init__(self, input, output_maps, input_maps, filter_height, filter_width, maxpool=None):
+    def __init__(self, input, output_maps, input_maps, filter_height, filter_width, image_shape, maxpool=None):
         self.input = input
-        self.bound = np.sqrt(6./(input_maps*filter_height*filter_width + output_maps*filter_height*filter_width))
-        self.w = theano.shared(np.asarray(np.random.uniform(low=-self.bound,high=self.bound,size=(output_maps, input_maps, filter_height, filter_width)),dtype=input.dtype))
-        self.b = theano.shared(np.asarray(np.random.uniform(low=-.5, high=.5, size=(output_maps)),dtype=input.dtype))
+        self.w = theano.shared(self.ortho_weights(output_maps,input_maps,filter_height,filter_width),borrow=True)
+        self.b = theano.shared(np.zeros((output_maps,), dtype=theano.config.floatX),borrow=True)
         self.conv_out = conv2d(input=self.input, filters=self.w, border_mode='half')
         if maxpool:
             self.conv_out = downsample.max_pool_2d(self.conv_out, ds=maxpool, ignore_border=True)
-        self.output = T.tanh(self.conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+        self.output = T.nnet.elu(self.conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+    def ortho_weights(self,chan_out,chan_in,filter_h,filter_w):
+        bound = np.sqrt(6./(chan_in*filter_h*filter_w + chan_out*filter_h*filter_w))
+        W = np.random.random((chan_out, chan_in * filter_h * filter_w))
+        u, s, v = np.linalg.svd(W,full_matrices=False)
+        if u.shape[0] != u.shape[1]:
+            W = u.reshape((chan_out, chan_in, filter_h, filter_w))
+        else:
+            W = v.reshape((chan_out, chan_in, filter_h, filter_w))
+        return W.astype(theano.config.floatX)
     def get_params(self):
         return self.w,self.b
 
@@ -39,22 +50,31 @@ class feedforward_layer(object):
     def __init__(self,input,features,nodes):
         self.input = input
         self.bound = np.sqrt(1.5/(features+nodes))
-        self.w = theano.shared(np.asarray(np.random.uniform(low=-self.bound,high=self.bound,size=(features,nodes)),dtype=theano.config.floatX))
-        self.b = theano.shared(np.asarray(np.random.uniform(low=-.5, high=.5, size=(nodes)),dtype=theano.config.floatX))
+        self.w = theano.shared(self.ortho_weights(features,nodes),borrow=True)
+        self.b = theano.shared(np.zeros((nodes,), dtype=theano.config.floatX),borrow=True)
         self.output = T.nnet.sigmoid(-T.dot(self.input,self.w)-self.b)
+    def ortho_weights(self,fan_in,fan_out):
+        bound = np.sqrt(2./(fan_in+fan_out))
+        W = np.random.randn(fan_in,fan_out)*bound
+        u, s, v = np.linalg.svd(W,full_matrices=False)
+        if u.shape[0] != u.shape[1]:
+            W = u
+        else:
+            W = v
+        return W.astype(theano.config.floatX)
     def get_params(self):
-        return self.w,self.b
+        return self.w,self.b        
 
 class neural_network(object):
     def __init__(self,convolutional_layers,feature_maps,filter_shapes,feedforward_layers,feedforward_nodes,classes):
-        self.input = T.tensor4()
+        self.input = T.tensor4()        
         self.convolutional_layers = []
-        self.convolutional_layers.append(convolutional_layer(self.input,feature_maps[1],feature_maps[0],filter_shapes[0][0],filter_shapes[0][1]))
+        self.convolutional_layers.append(convolutional_layer(self.input,feature_maps[1],feature_maps[0],filter_shapes[0][0],filter_shapes[0][1],image_shapes[0]))
         for i in range(1,convolutional_layers):
             if i==2 or i==4:
-                self.convolutional_layers.append(convolutional_layer(self.convolutional_layers[i-1].output,feature_maps[i+1],feature_maps[i],filter_shapes[i][0],filter_shapes[i][1],maxpool=(2,2)))
+                self.convolutional_layers.append(convolutional_layer(self.convolutional_layers[i-1].output,feature_maps[i+1],feature_maps[i],filter_shapes[i][0],filter_shapes[i][1],image_shapes[i],maxpool=(2,2)))
             else:
-                self.convolutional_layers.append(convolutional_layer(self.convolutional_layers[i-1].output,feature_maps[i+1],feature_maps[i],filter_shapes[i][0],filter_shapes[i][1]))
+                self.convolutional_layers.append(convolutional_layer(self.convolutional_layers[i-1].output,feature_maps[i+1],feature_maps[i],filter_shapes[i][0],filter_shapes[i][1],image_shapes[i]))
         self.feedforward_layers = []
         self.feedforward_layers.append(feedforward_layer(self.convolutional_layers[-1].output.flatten(2),11520,feedforward_nodes[0]))
         for i in range(1,feedforward_layers):
@@ -118,11 +138,11 @@ class neural_network(object):
 print "building neural network"
 nn = neural_network(convolutional_layers,feature_maps,filter_shapes,feedforward_layers,feedforward_nodes,classes)
 
-batch_size = 250
+batch_size = 100
 
-for i in range(20000):
+for i in range(80000):
     cost = nn.train(X_train,y_train,batch_size)
-    sys.stdout.write("step %i training error: %f \r" % (i+1, cost))
+    sys.stdout.write("step %i loss: %f \r" % (i+1, cost))
     sys.stdout.flush()
     if (i+1)%100 == 0:
         preds = []
@@ -130,4 +150,4 @@ for i in range(20000):
              preds.append(nn.predict(X_test[j:j+batch_size,:]))
         pred = np.concatenate(preds)
         error = 1-float(np.sum(pred==y_test))/len(pred)
-        print "error at iteration %i: %.4f" % (i+1,error)
+        print "test error at iteration %i: %.4f" % (i+1,error)
